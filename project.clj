81e7ff0a96c39f743e5d085b143f1108c4b3f20e
Ethan Miller  - Development Server <ethanzanemiller@gmail.com>
2017-02-06 09:58:43 +0200

    Add more ring setup

--- project.clj
+++ project.clj
@@ -6,4 +6,11 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [ring/ring-core "1.5.1"]
                  [ring/ring-jetty-adapter "1.5.1"]
-                 [compojure "1.5.2"]])
+                 [compojure "1.5.2"]]
+  :plugins [[lein-ring "0.11.0"]]
+  :ring {:handler datemo.handler/app
+         :nrepl {:start? true
+                 :port 9998}}
+  :profiles
+  {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
+                        [ring/ring-mock "0.3.0"]]}})
