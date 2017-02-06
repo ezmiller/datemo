f4d7a0ebbeac5091c6d68c21123205fd2b0da3b7
Ethan Miller  - Development Server <ethanzanemiller@gmail.com>
2017-02-06 09:45:13 +0200

    Add ring server dependencies

--- project.clj
+++ project.clj
@@ -3,4 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [ring/ring-core "1.5.1"]
+                 [ring/ring-jetty-adapter "1.5.1"]
+                 [compojure "1.5.2"]])
