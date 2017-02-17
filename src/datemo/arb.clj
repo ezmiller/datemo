1969b008d15ce182dc2e1592014a3bbd7ffbf117
Ethan Miller  - Development Server <ethanzanemiller@gmail.com>
2017-02-17 09:52:58 +0200

    Change hiccup->arb to simpler syntax

--- src/datemo/arb.clj
+++ src/datemo/arb.clj
@@ -6,20 +6,18 @@
   (as-hiccup (parse html)))
 
 (defn hiccup->arb [hiccup]
-  (let [[tag attrs & rest] (first hiccup)
-        rest-count (count rest)]
-    (if (or (= 0 rest-count) (and (= 1 rest-count) (string? (first rest))))
-      [:arb {:original-tag tag} (first rest)]
-      (loop [arb [:arb {:original-tag tag}]
-             forms rest]
-        (if (= 1 (count forms))
-          (conj arb (if (string? (first forms))
-                      (first forms)
-                      (hiccup->arb forms)))
-          (recur (conj arb (if (string? (first forms))
-                             (first forms)
-                             (hiccup->arb (list (first forms)))))
-                 (next forms)))))))
+  (if (string? (first hiccup))
+    (first hiccup)
+    (let [[tag attrs & rest] (first hiccup)
+          rest-count (count rest)]
+      (if (or (= 0 rest-count) (and (= 1 rest-count) (string? (first rest))))
+        [:arb {:original-tag tag} (first rest)]
+        (loop [arb [:arb {:original-tag tag}]
+               forms rest]
+          (if (= 1 (count forms))
+            (conj arb (hiccup->arb forms))
+            (recur (conj arb (hiccup->arb (list (first forms))))
+                   (next forms))))))))
 
 (defn html->arb [html]
   (hiccup->arb (html->hiccup html)))
