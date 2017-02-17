cefbf5b7081fdb971ec3bce83b892b27ff47767a
Ethan Miller  - Development Server <ethanzanemiller@gmail.com>
2017-02-17 09:49:08 +0200

    Add and pass more tests for html->arb

--- src/datemo/arb.clj
+++ src/datemo/arb.clj
@@ -6,13 +6,20 @@
   (as-hiccup (parse html)))
 
 (defn hiccup->arb [hiccup]
-  (let [[tag attrs & rest] (first hiccup)]
-    (if (or (= 0 (count rest)) (string? (first rest)))
+  (let [[tag attrs & rest] (first hiccup)
+        rest-count (count rest)]
+    (if (or (= 0 rest-count) (and (= 1 rest-count) (string? (first rest))))
       [:arb {:original-tag tag} (first rest)]
-      (loop [arb [:arb {:original-tag tag}] forms rest]
+      (loop [arb [:arb {:original-tag tag}]
+             forms rest]
         (if (= 1 (count forms))
-          (conj arb (hiccup->arb forms))
-          (recur (conj arb (hiccup->arb (list (first forms)))) (next forms)))))))
+          (conj arb (if (string? (first forms))
+                      (first forms)
+                      (hiccup->arb forms)))
+          (recur (conj arb (if (string? (first forms))
+                             (first forms)
+                             (hiccup->arb (list (first forms)))))
+                 (next forms)))))))
 
 (defn html->arb [html]
   (hiccup->arb (html->hiccup html)))
--- test/datemo/arb_test.clj
+++ test/datemo/arb_test.clj
@@ -4,22 +4,44 @@
         hiccup.core
         datemo.arb))
 
-(defn long-str [& strings] (clojure.string/join "\n" strings))
-
 (deftest test-arb
   (testing "html->arb"
     ;; these two tests need to be adapted to full document output
     ;; that comes from hickory when using the parse instead of 
     ;; parse-fragment method. need to decide which is  better to use.
-    ;; (is (= (html->arb "<div></div>") [:arb {:original-tag :div} nil]))
-    ;; (is (= (html->arb "<div>Text</div>") [:arb {:original-tag :div} "Text"]))
     (is (=
-         (html->arb "<div>A</div><div>B</div>")
+         (html->arb (html [:div]))
+         [:arb {:original-tag :html}
+          [:arb {:original-tag :head} nil]
+          [:arb {:original-tag :body}
+           [:arb {:original-tag :div} nil]]]))
+    (is (=
+         (html->arb (html [:div {} "Text"]))
+         [:arb {:original-tag :html}
+          [:arb {:original-tag :head} nil]
+          [:arb {:original-tag :body}
+           [:arb {:original-tag :div} "Text"]]]))
+    (is (=
+         (html->arb (html [:div "A"] [:div "B"]))
          [:arb {:original-tag :html}
           [:arb {:original-tag :head} nil]
           [:arb {:original-tag :body}
            [:arb {:original-tag :div} "A"]
            [:arb {:original-tag :div} "B"]]]))
-  ))
+    (is (=
+         (html->arb (html [:h1 "Section"] [:p "This is a paragraph."]))
+         [:arb {:original-tag :html}
+          [:arb {:original-tag :head} nil]
+          [:arb {:original-tag :body}
+           [:arb {:original-tag :h1} "Section"]
+           [:arb {:original-tag :p} "This is a paragraph."]]]))
+    (is (= (html->arb (html [:p "Paragraph with " [:strong "bold"] " text."]))
+           [:arb {:original-tag :html}
+            [:arb {:original-tag :head} nil]
+            [:arb {:original-tag :body}
+             [:arb {:original-tag :p}
+              "Paragraph with "
+              [:arb {:original-tag :strong} "bold"]
+              " text."]]]))))
 
 
