11701a31452d71c22145f4f4fa0e1faace144b7a
Ethan Miller  - Development Server <ethanzanemiller@gmail.com>
2017-02-22 10:59:23 +0200

    Add :main in profile

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :main datemo.core
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [cheshire "5.7.0"]
                  [ring/ring-core "1.5.1"]
