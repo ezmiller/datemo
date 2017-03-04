05a84abb4ddbfb85474ee9d8a05f4198dced2991
Ethan Miller  - Development Server <ethanzanemiller@gmail.com>
2017-03-04 09:28:49 +0200

    Fix routes tests

--- test/datemo/routes_test.clj
+++ test/datemo/routes_test.clj
@@ -9,9 +9,9 @@
     (let [response (app (request :get "/"))]
       (is (= (:status response) 200))
       (is (= (json/parse-string (:body response) true)
-             {:_links {:documents {:href "/docs"}}})))))
+             {:_links {:documents {:href "/docs"}}}))))
 
   (testing "not-found route"
     (let [response (app (request :get "/bogus/route"))]
       (is (= (:status response) 404))
-      (is (= (:body "Not found")))))
+      (is (= (:body response) "Not found")))))
