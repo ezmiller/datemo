b8bf2e0283a9d0ae5bb577759485e9e149d77fa4
Ethan Miller  - Development Server <ethanzanemiller@gmail.com>
2017-03-05 19:58:57 +0200

    Add partly working POST /documents route

--- src/datemo/routes.clj
+++ src/datemo/routes.clj
@@ -1,12 +1,56 @@
 (ns datemo.routes
-  (:use compojure.core)
-  (:require [compojure.route :as route]
+  (:use compojure.core
+        hiccup.core
+        datemo.arb)
+  (:require [clojure.edn :as edn]
+            [compojure.route :as route]
             [compojure.handler :as handler]
             [ring.middleware.json :refer [wrap-json-response]]
-            [ring.middleware.defaults :refer [wrap-defaults api-defaults]]))
+            [ring.middleware.defaults :refer [wrap-defaults api-defaults]]
+            [datomic.api :as d]))
+
+(require '[clojure.pprint :refer [pprint]])
+
+(def db-uri "datomic:dev://localhost:4334/datemo")
+(def conn (d/connect db-uri))
+(def db (d/db conn))
+
+(defn save-arb-tx [tx]
+  "Transact and return entity id and reference to db in new state."
+  [conn tx]
+  (let [tempid (:db/id tx)
+        post-tx @(d/transact conn [tx])
+        db-after (:db-after post-tx)
+        eid (d/resolve-tempid db (:tempids post-tx) tempid)]
+    [eid db-after]))
+
+(defn pull-arb-by-eid [eid]
+  (d/pull (d/db conn) '[*] eid))
+
+;; I tried to use :db.part/arb for the tempid below, but it
+;; caused an error, invalid id. 
+(defn add-tempid [arb-tx]
+  (into arb-tx {:db/id (d/tempid :db.part/user)}))
+
+;; Note: You get an error if the {:readers *data-reader*} bit is not added.
+;; This seems to relate to the need for data-readers to understand certain
+;; tags; in this case: :db/id. Datomic installs some data readers for us.
+;; But we need to do this to get them picked up. See here for a bit more
+;; info: https://clojure.org/reference/reader#_tagged_literals.
+(defn edn->clj [edn]
+  (edn/read-string {:readers *data-readers*} (prn-str edn)))
+
+(defn post-doc [doc-string]
+ (let [tx (-> (html->tx doc-string) (add-tempid) (edn->clj))
+       [eid db-after] (save-arb-tx tx)
+       tx-from-db (d/pull db-after '[*] eid)
+       html (-> tx-from-db (tx->arb) (arb->hiccup) (html))]
+   {:body {:_links {:href (apply str "/documents/" (str eid))}
+           :_embedded {:html html}}}))
 
 (defroutes app-routes
   (GET "/" [] {:body {:_links {:documents {:href "/docs"}}}})
+  (POST "/documents" [doc-string] (post-doc doc-string))
   (route/not-found "Not found"))
 
 (def app
--- test/datemo/routes_test.clj
+++ test/datemo/routes_test.clj
@@ -2,7 +2,13 @@
   (:require [cheshire.core :as json :refer [parse-string]])
   (:use clojure.test
         ring.mock.request
+        markdown.core
         datemo.routes))
+
+(use '[clojure.pprint :refer [pprint]])
+
+(defn parse-response [response]
+  (-> (:body response) (json/parse-string true)))
 
 (deftest test-app
   (testing "GET /"
@@ -10,6 +16,20 @@
       (is (= (:status response) 200))
       (is (= (json/parse-string (:body response) true)
              {:_links {:documents {:href "/docs"}}}))))
+
+  (testing "POST /documents"
+    (def data
+      (array-map :doc-string (md-to-html-string "# Title  \nParagraph")))
+    (let [response (app (request :post "/documents" data))]
+      (is (= 200 (:status response)))
+      (is (= "/documents/"
+             (-> (parse-response response)
+                 (:_links)
+                 (->> (:href) (re-find #"/documents/")))))
+      (is (=
+           {:html "<div><h1>Title</h1><p>Paragraph</p></div>"}
+           (-> (parse-response response)
+               (:_embedded))))))
 
   (testing "not-found route"
     (let [response (app (request :get "/bogus/route"))]
