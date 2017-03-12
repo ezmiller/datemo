94a6957af9789da104e43fcac920378cf65dfc44
Ethan Miller  - Development Server <ethanzanemiller@gmail.com>
2017-03-12 15:41:47 +0200

    Clean up core
    Just holds code useful for rerunning taxonomy.

--- src/datemo/core.clj
+++ src/datemo/core.clj
@@ -3,19 +3,8 @@
   (:require
     [datomic.api :as d]
     [clojure.java.io :as io]
-    [clojure.pprint :as pp :refer (pprint)])
-  (:gen-class))
+    [clojure.pprint :as pp :refer (pprint)]))
 
-;; (def conn
-;;   (<!! (client/connect
-;;         {:db-name "datemo"
-;;          :account-id client/PRO_ACCOUNT
-;;          :secret "datemo"
-;;          :region "none"
-;;          :endpoint "localhost:8998"
-;;          :service "peer-server"
-;;          :access-key "datemo"})))
-;; (def db (client/db conn))
 (def db-uri "datomic:dev://localhost:4334/datemo");
 (def conn (d/connect db-uri))
 (def db (d/db conn))
@@ -37,15 +26,7 @@
              more)
       {:datoms n})))
 
-(defn -main []
-  (def tx (-> (io/resource "sample-docs.edn")
-               (read-all)
-               (first)))
-  (def schema-tx (-> (io/resource "schemas/arb.edn")
-                     (read-all)
-                     (first)))
-  ;; (pprint (d/transact conn schema-tx))
-  ;; (pprint (d/transact conn tx))
-  ;; (pprint (into {} (d/entity db 17592186045418)))
-  ;; (pprint (d/pull db '[*] 17592186045418))
-  )
+;; (def schema-tx (-> (io/resource "schemas/arb.edn")
+;;                    (read-all)
+;;                    (first)))
+;; (pprint (d/transact conn schema-tx))
