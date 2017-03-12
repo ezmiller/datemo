1e59397ca78be790d2637b5db9b5bddf94f56289
Ethan Miller  - Development Server <ethanzanemiller@gmail.com>
2017-03-12 15:41:04 +0200

    Hook up ring server
    This took FOREVER because there was a conflict in the transitive dependencies.
    So I had to do a global exclusion for the offending package: commons-codec, and
    then include the specific one needed by ring. Used lean deps :tree to figure this out.

--- project.clj
+++ project.clj
@@ -4,13 +4,13 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :main datemo.core
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :exclusions [[commons-codec]]
+  :dependencies [[commons-codec "1.6"]
+                 [org.clojure/clojure "1.8.0"]
                  [org.clojure/core.async "0.2.395"]
-                 [com.datomic/clj-client "0.8.606"]
                  [com.datomic/datomic-pro "0.9.5544"]
                  [cheshire "5.7.0"]
-                 [ring/ring-core "1.5.1"]
-                 [ring/ring-jetty-adapter "1.5.1"]
+                 [ring "1.5.1"]
                  [ring/ring-defaults "0.2.3"]
                  [ring/ring-json "0.4.0"]
                  [compojure "1.5.2"]
@@ -18,7 +18,7 @@
                  [markdown-clj "0.9.94"]]
   :plugins [[lein-ring "0.11.0"]
             [com.jakemccrary/lein-test-refresh "0.18.1"]]
-  :ring {:handler datemo.handler/app
+  :ring {:handler datemo.routes/app
          :nrepl {:start? true
                  :port 9998}}
   :test-paths ["test"]
