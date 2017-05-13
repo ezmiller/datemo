520d0fa85458c07f9e7b15cb530a410c196c9d2c
Ethan Miller  - Development Server <ethanzanemiller@gmail.com>
2017-05-13 22:42:00 +0300

    Add correct install for atomic

--- project.clj
+++ project.clj
@@ -4,11 +4,14 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :exclusions [[commons-codec]]
+  :repositories {"my.datomic.com" {:url "https://my.datomic.com/repo"
+                                   :creds :gpg}}
   :dependencies [[clj-time "0.13.0"]
                  [commons-codec "1.6"]
                  [org.clojure/clojure "1.8.0"]
                  [org.clojure/core.async "0.2.395"]
                  [com.datomic/datomic-pro "0.9.5544"]
+                 [com.amazonaws/aws-java-sdk-dynamodb "1.11.6"]
                  [environ "1.1.0"]
                  [cheshire "5.7.0"]
                  [ring "1.5.1"]
@@ -28,7 +31,8 @@
                  :port 9998}}
   :test-paths ["test"]
   :profiles
-  {:production {:dependencies [[javax.serverlet/servlet-api "2.5"]
+  {:production {:env {:port 8080}
+                :dependencies [[javax.serverlet/servlet-api "2.5"]
                                [ring/ring-mock "0.3.0"]]}
    :dev {:env {:database-uri "datomic:dev://localhost:4334/datemo"}
          :dependencies [[javax.servlet/servlet-api "2.5"]
