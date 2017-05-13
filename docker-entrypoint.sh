c0991064348fd583b5c7c89063e129fbf6783e2e
Ethan Miller  - Development Server <ethanzanemiller@gmail.com>
2017-05-13 22:41:02 +0300

    Add prelim docker setup

---
+++ docker-entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Reading secrets..."
+read -d $'\x04' aws_access_key < "/aws_access_key"
+read -d $'\x04' aws_secret_access_key < "/aws_secret_access_key"
+echo "Done"
+
+database_uri="datomic:ddb://us-east-1/datemo/test-db?aws_access_key_id=$aws_access_key&aws_secret_key=$aws_secret_access_key"
+
+echo "Starting datemo..."
+java -DDATABASE.URI=$database_uri -jar /datemo-0.1.0-SNAPSHOT-standalone.jar
