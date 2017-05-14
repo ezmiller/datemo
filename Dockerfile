4cc94cbd9575ef6dd9f97f47f37b243aa3ba6cde
Ethan Miller  - Development Server <ethanzanemiller@gmail.com>
2017-05-14 10:32:49 +0300

    Add working docker-compose config for prod

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM openjdk:8
 ADD ./docker-entrypoint.sh /
 ADD ./target/datemo-0.1.0-SNAPSHOT-standalone.jar /
 RUN /bin/bash -c 'chmod u+x /docker-entrypoint.sh'
-ENV PORT=80
-EXPOSE 80
+ENV PORT=8080
+EXPOSE 8080
 CMD ["sh", "-c", "/docker-entrypoint.sh"]
 
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 echo "Reading secrets..."
-read -d $'\x04' aws_access_key < "/aws_access_key"
-read -d $'\x04' aws_secret_access_key < "/aws_secret_access_key"
+read -d $'\x04' aws_access_key < "/run/secrets/aws_access_key"
+read -d $'\x04' aws_secret_access_key < "/run/secrets/aws_secret_access_key"
 echo "Done"
 
 database_uri="datomic:ddb://us-east-1/datemo/test-db?aws_access_key_id=$aws_access_key&aws_secret_key=$aws_secret_access_key"
