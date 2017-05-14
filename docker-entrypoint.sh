14898c45297f452308234676e6d9b4b5d029ba87
Ethan Miller  - Development Server <ethanzanemiller@gmail.com>
2017-05-14 11:40:53 +0300

    Make db-uri settable in docker-compose

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,7 +5,7 @@ read -d $'\x04' aws_access_key < "/run/secrets/aws_access_key"
 read -d $'\x04' aws_secret_access_key < "/run/secrets/aws_secret_access_key"
 echo "Done"
 
-database_uri="datomic:ddb://us-east-1/datemo/test-db?aws_access_key_id=$aws_access_key&aws_secret_key=$aws_secret_access_key"
+database_uri="$DATEMO_DB_URI?aws_access_key_id=$aws_access_key&aws_secret_key=$aws_secret_access_key"
 
 echo "Starting datemo..."
 java -DDATABASE.URI=$database_uri -jar /datemo-0.1.0-SNAPSHOT-standalone.jar
