0ff27bba416235ce0a6140a51956e1cf20757cf4
Ethan Miller  - Development Server <ethanzanemiller@gmail.com>
2018-06-02 16:06:53 -0400

    Get tests to pass after upgrade to datomic cloud

---
+++ repl.clj
@@ -0,0 +1,16 @@
+(require
+ '[clojure.data.csv :as csv]
+ '[clojure.data.json :as json]
+ '[clojure.java.io :as io]
+ '[clojure.java.shell :as sh]
+ '[clojure.main :as main]
+ '[clojure.pprint :as pp]
+ '[clojure.spec.alpha :as s]
+ '[clojure.string :as str]
+ '[datomic.dodc.repl-ui :as ui :refer (label! table! spreadsheet! tree!)]
+ '[datomic.dodc.repl-ui.specs :as ui-specs]
+ '[datomic.client.api :as d]
+ '[seesaw.core :as ss]
+ '[seesaw.table :as table]
+ '[seesaw.tree :as tree])
+(set! *print-length* 25)
