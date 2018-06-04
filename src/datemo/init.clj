aa07f382b5feaac269dba212edcf53f2b2fcec0f
Ethan Miller  - Development Server <ethanzanemiller@gmail.com>
2018-06-04 09:25:02 -0400

    Modify init for datomic cloud

--- src/datemo/init.clj
+++ src/datemo/init.clj
@@ -1,9 +1,23 @@
 (ns datemo.init
-  (require
+  (:require
+    [environ.core :refer [env]]
     [datemo.db :as db]))
 
 (require '[clojure.pprint :refer [pprint]])
 
+(defn prep-db [name]
+  (println "Database `" name "` exists?: " (db/db-exists name))
+  (if (db/db-exists name)
+    (db/connect name)
+    (do
+      (println "Database `" name "` does not exist, will set it up...")
+      (db/create-db name)
+      (db/connect name)
+      (-> (db/load-schema "schemas/arb.edn")
+          (db/install-schema (db/get-conn))))))
+
 (defn init []
   (println (apply str "Initializing datemo version: " (:datemo-version env)))
-  (db/init-db cfg))
+  (db/init-client)
+  (if (not (:testing env))
+    (prep-db (:db-name env))))
