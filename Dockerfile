72d467f448d2709c65f773a6ad9f910006becae2
Ethan Miller  - Development Server <ethanzanemiller@gmail.com>
2018-09-29 14:28:19 -0400

    Run datomic-socks-proxy in datemo container
    For now, I'll get access to the database this way, until I setup the app
    as a Datomic Ion.

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,36 @@
-FROM openjdk:8
+FROM alpine:3.7
+
+RUN apk update \
+&& apk upgrade \
+&& apk add --no-cache bash \
+&& apk add --no-cache curl \
+&& apk add --no-cache openssh \
+&& apk add --no-cache openjdk8-jre
+
+RUN apk add --no-cache python3 \
+&& python3 -m ensurepip \
+&& pip3 install --upgrade pip setuptools \
+&& rm -r /usr/lib/python*/ensurepip && \
+if [ ! -e /usr/bin/pip ]; then ln -s pip3 /usr/bin/pip ; fi && \
+if [[ ! -e /usr/bin/python ]]; then ln -sf /usr/bin/python3 /usr/bin/python; fi && \
+rm -r /root/.cache
+
+# Datomic SOCKS proxy to get to the Datomic DB in the cloud
+# Eventually this whole app could be run as a Datomic Ion instead.
+RUN wget -O datomic-socks-proxy 'https://docs.datomic.com/cloud/files/datomic-socks-proxy' && \
+    chmod +x datomic-socks-proxy
+
+# datomic-socks-proxy requires AWS CLI
+RUN pip3 install awscli --upgrade
+
 ADD ./docker-entrypoint.sh /
 RUN /bin/bash -c 'chmod u+x /docker-entrypoint.sh'
+
+# Add datemo app jar file
 ADD ./target/datemo-0.2-standalone.jar /
+
 ENV PORT=8080
 EXPOSE 8080
+
 CMD ["sh", "-c", "/docker-entrypoint.sh"]
 
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,51 @@
 #!/bin/bash
 
-echo "Running datomic SOCKS proxy..."
-./datomic-socks-proxy -p us-east-2 datemo &
+echo "Reading secrets..."
+read -d $'\x04' AWS_ACCESS_KEY < "/run/secrets/aws_access_key"
+read -d $'\x04' AWS_SECRET_ACCESS_KEY < "/run/secrets/aws_secret_access_key"
+echo "Done"
+
+mkdir ~/.aws
+cat > ~/.aws/credentials << EOF
+[default]
+aws_access_key_id = $AWS_ACCESS_KEY
+aws_secret_access_key = $AWS_SECRET_ACCESS_KEY
+EOF
+
+cat > ~/.aws/config << EOF
+[default]
+region = us-east-2
+EOF
+
+# Hack to prevent ssh from trying to check host keys when the
+# datomic SOCKS script runs. I think this is okay. Note the option
+# 'AddressFamily inet' prevents ssh from trying to use ipv6 to connect
+# which was prevening binding or some reason.
+mkdir /root/.ssh
+cat > /root/.ssh/config << EOF
+Host *
+  StrictHostKeyChecking no
+  UserKnownHostsFile=/dev/null
+  AddressFamily inet
+EOF
+
+echo "Starting Datomic SOCKS proxy..."
+./datomic-socks-proxy -p default $DATOMIC_CLOUD_STACK_NAME &
+
+echo "Waiting for SOCKS proxy..."
+
+while true
+do
+  STATUS=$(curl -s -o /dev/null -w '%{http_code}' -x socks5h://localhost:8182 http://entry.datemo.us-east-2.datomic.net:8182/)
+  if [ $STATUS -eq 200 ]; then
+    echo "Datomic SOCKS Proxy ready!"
+    break
+  else
+    echo "Got $STATUS :( Not ready yet..."
+    echo $OUTPUT
+  fi
+  sleep 10
+done
 
 echo "Starting datemo..."
-java -Ddatabase.db-name="production" -jar datemo-0.2-standalone.jar
+java -Ddb-name="production" -jar datemo-0.2-standalone.jar
