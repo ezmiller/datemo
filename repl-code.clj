bd1f3bb9ceea4860a81fab9f82b5e1c242e73740
Ethan Miller  - Development Server <ethanzanemiller@gmail.com>
2019-01-09 17:28:10 -0500

    Update repl-code to simpler set
    Goes with new init logic

--- repl-code.clj
+++ repl-code.clj
@@ -1,46 +1,10 @@
-;; For repl.
+;; For repl, to get working with db.
 
-;; Sample arb doc in edn
-;;
-;; [{:arb/metadata [{:html-tag :body}]
-;;   :arb/value [{:arb/metadata [{:html-tag :h1}]
-;;                :arb/value [{:text "Title"}]}
-;;               {:arb/metadata [{:html-tag :p}]
-;;                :arb/value [{:text "This is a paragraph."}]}]}]
-
-
-;; for client api
-(require '[clojure.core.async :refer [<!!]]
-         '[datomic.client :as client])
-
-(def conn
-  (<!! (client/connect
-        {:db-name "datemo"
-         :account-id client/PRO_ACCOUNT
-         :secret "datemo"
-         :region "none"
-         :endpoint "localhost:8998"
-         :service "peer-server"
-         :access-key "datemo"})))
-
-(def db (client/db conn))
-
-;;
-(d/delete-database "datomic:dev://localhost:4334/datemo")
-(d/create-database "datomic:dev://localhost:4334/datemo")
-
-
-;; for datomic api
-(require '[datomic.api :as d])
-
-(def conn (d/connect "datomic:dev://localhost:4334/datemo"))
-(def db-now (d/db conn))
-
-;; for installing the schema
-;; run lein-repl, then:
+(require '[datomic.client.api :as d])
 (use 'datemo.db)
-(require '[datomic.api :as d])
-(init-db)
-(-> (load-schema "schemas/arb.edn")
-    (install-schema (get-conn)))
+(init-client)
+(connect "development") ;; can also connect "production"
+
+;; At this point you can use the fns in datemo.db to work with the db.
+
 
